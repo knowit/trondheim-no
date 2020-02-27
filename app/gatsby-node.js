6081c031462533bb57e121d276847783ef25c3ef
Levi Sørum <levi.sorum@gmail.com>
2020-02-27 10:51:29 +0100

    Added static site generation.

--- app/gatsby-node.js
+++ app/gatsby-node.js
@@ -5,3 +5,44 @@
  */
 
 // You can delete this file if you're not using it
+const path = require(`path`)
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allFlamelinkArticleContent {
+        edges {
+          node {
+            slug
+            parentContent {
+              slug
+            }
+          }
+        }
+      }
+      allFlamelinkListingPageContent {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allFlamelinkListingPageContent.edges.forEach(({ node }) => {
+    createPage({
+      path: node.slug,
+      component: path.resolve(`./src/templates/listing-page.js`),
+    })
+  })
+
+  result.data.allFlamelinkArticleContent.edges.forEach(({ node }) => {
+    createPage({
+      path: node.parentContent.slug + "/" + node.slug,
+      component: path.resolve(`./src/templates/listing-page.js`),
+    })
+  })
+
+}
