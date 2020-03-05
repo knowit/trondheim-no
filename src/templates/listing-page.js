88b37d4a71013be0b0b9de9249412f90d4b95ad4
Levi Sørum <levi.sorum@gmail.com>
2020-03-05 12:55:34 +0100

    Added article.js, listing-page.js, and listing-page.css

--- src/templates/listing-page.js
+++ src/templates/listing-page.js
@@ -1,6 +1,78 @@
 import React from "react"
-export default () => {
-  return (
-    <div></div>
-  )
-}
+import { StaticQuery, graphql } from "gatsby"
+import "../style/listing-page.css"
+
+
+const ListingPage = () => (
+  <StaticQuery
+    query={graphql`
+    query {
+      allFlamelinkArticleContent(filter: {parentContent: {slug: {eq: "severdig"}}}) {
+        edges {
+          node {
+            slug
+            parentContent {
+              slug
+              localTitle
+            }
+            openingHours
+            tags
+            thumbnail {
+              url
+            }
+            title
+          }
+        }
+      }
+      allFlamelinkListingPageContent(filter: {slug: {eq: "severdig"}}) {
+        edges {
+          node {
+            slug
+            localTitle
+            textOnPage
+          }
+        }
+      }
+    }
+    
+    
+  `}
+    render={data => (
+      <div id="outer-container">
+        <div id="inner-container">
+
+          <div id="articles-header">
+            <h2>{data.allFlamelinkListingPageContent.edges[0].node.localTitle}</h2>
+              <p>{data.allFlamelinkListingPageContent.edges[0].node.textOnPage}</p>
+          </div>
+
+          <div id="articles-container">
+
+            {data.allFlamelinkArticleContent.edges.map(function (node, key) {
+              console.log(node)
+              return (
+                <div class="article-container">
+                  <div class="article-thumbnail" style={{ backgroundImage: "url(" + node.node.thumbnail[0].url + ")" }}></div>
+                  <h2><a href={node.node.parentContent.slug + "/" + node.node.slug}>{node.node.title}</a></h2>
+                  <div class="tags-container">
+                    {node.node.tags.map(function (tag, key) {
+                      return (
+                        <div class="tag">
+                          <a>{tag}</a>
+                        </div>
+                      )
+                    })}
+                  </div>
+                </div>
+              )
+            })}
+
+          </div>
+
+        </div>
+      </div>
+    )}
+  />
+)
+
+export default ListingPage
