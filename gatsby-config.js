cbb64b5eaf96ff60a975cb6bac9f4a4380ee0f33
Levi Sørum <levi.sorum@gmail.com>
2020-04-02 16:05:34 +0200

    Added some plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,19 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
+    {
       resolve: 'gatsby-source-flamelink',
       options: {
         firebaseConfig: {
