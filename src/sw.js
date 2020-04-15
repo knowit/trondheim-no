915a784bc8fecf18e1a0294f49653cf2453ea8c6
Levi Sørum <levi.sorum@gmail.com>
2020-04-15 16:01:16 +0200

    test

--- src/sw.js
+++ src/sw.js
@@ -23,7 +23,7 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-  console.log('Service Worker: Fetching ' + event.request);
+  console.log('Service Worker: Fetching ' + JSON.stringify(event.request));
   event.respondWith(
     caches.match(event.request)
       .then(function (response) {
