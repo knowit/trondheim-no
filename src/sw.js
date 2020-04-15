eee1c7bdad5c31916bf0df33d3a6d7832c739988
Levi Sørum <levi.sorum@gmail.com>
2020-04-15 15:04:59 +0200

    test

--- src/sw.js
+++ src/sw.js
@@ -1,24 +1,20 @@
-const cacheName = 'pages-cache-v1';
+
+var static_urls = [
+  'https://trondheim.no/images/severdig/bryggene-2.png',
+  'https://trondheim.no/images/severdig/bryggene-3.png'
+];
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      console.log("Cached stuff")
-      return cache.addAll(
-        [
-          '^https://trondheim.no/images/severdig/bryggene-2.png',
-        ]
-      );
-    }
-
-    )
-  );
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+    caches.open('external-cache').then(function (cache) {
+      cache.addAll(static_urls.map(function (urlToPrefetch) {
+        console.log(urlToPrefetch);
+        return new Request(urlToPrefetch, { mode: 'cors' });
+      })).catch(function (error) {
+        console.error(error);
+      }).then(function () {
+        console.log('All fetched and cached');
+      });
     })
   );
 });
