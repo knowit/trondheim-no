f3cf060b8f5db8ecc70ff6965a07e5e35cfc9026
Levi Sørum <levi.sorum@gmail.com>
2020-04-15 11:20:14 +0200

    Added second workbox url precache to service worker.

--- src/service-worker.js
+++ src/service-worker.js
@@ -9,3 +9,17 @@ workbox.routing.registerRoute(
     ]
   })
 );
+
+workbox.routing.registerRoute(
+  new RegExp('^https://firebasestorage.googleapis.com/v0/b/trondheimno-demo.appspot.com/o/flamelink%2Fmedia%2F'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      })
+    ]
+  })
+);
+
+
