66947839d296b068f150a72fcc288fbe4b70059d
Levi Sørum <levi.sorum@gmail.com>
2020-04-15 11:33:09 +0200

    Removed second workbox url

--- src/service-worker.js
+++ src/service-worker.js
@@ -10,16 +10,3 @@ workbox.routing.registerRoute(
   })
 );
 
-workbox.routing.registerRoute(
-  new RegExp('^https://firebasestorage.googleapis.com/v0/b/trondheimno-demo.appspot.com/o/flamelink%2Fmedia%2F'),
-  new workbox.strategies.CacheFirst({
-    cacheName: 'image-cache',
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200],
-      })
-    ]
-  })
-);
-
-
