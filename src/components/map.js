cae1f71bc762988c6e7f0db37ac776580503a710
Levi Sørum <levi.sorum@gmail.com>
2020-04-23 11:06:01 +0200

    Testing offline rendering

--- src/components/map.js
+++ src/components/map.js
@@ -27,28 +27,36 @@ class Map extends Component {
 
         if (!this.props.location) return "";
 
-        const MapView = withScriptjs(withGoogleMap(props => (
-            <div style={{ position: "relative" }}>
+        const MapView =
+
+            <div>
                 <Online>
-                    {this.createPersistentGoogleLink()}
-                    <GoogleMap
-                        defaultCenter={this.props.location}
-                        defaultZoom={17}
-                    >
-                        <Marker position={this.props.location} />
-                    </GoogleMap>
+                    {withScriptjs(withGoogleMap(props => (
+                        <div style={{ position: "relative" }}>
+                            {this.createPersistentGoogleLink()}
+                            <GoogleMap
+                                defaultCenter={this.props.location}
+                                defaultZoom={17}
+                            >
+                                <Marker position={this.props.location} />
+                            </GoogleMap>
+                        </div>
+                    )))}
                 </Online>
             </div>
-        )));
+
+
         return (
             <div className={styles.mapContainer}>
                 <Online>
+                    Map is online
+                    {/*
                     <MapView
                         containerElement={<div style={{ height: `400px`, width: '100%' }} />}
                         mapElement={<div style={{ height: `100%` }} />}
                         googleMapURL={"https://maps.googleapis.com/maps/api/js?key=" + process.env.GATSBY_GOOGLE_API + "&v=3.exp&libraries=geometry,drawing,places"}
                         loadingElement={<div style={{ height: `100%` }} />}
-                    />
+                    />*/}
                 </Online>
                 <Offline>
                     Maps is offline
