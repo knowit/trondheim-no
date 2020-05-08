38ed2c94e02f78758e5d6a0f59c40808ef0b6d0b
Levi Sørum <levi.sorum@gmail.com>
2020-05-08 16:01:13 +0200

    Added rendering of nested listing pages.

--- src/templates/listing-page.js
+++ src/templates/listing-page.js
@@ -50,6 +50,18 @@ const ListingPage = ({ pageContext }) => {
 
 
           <div id="articles-container">
+
+            {pageContext.subListingPages.map(function (node, key) {
+              return (
+                <div class="article-container">
+                  <Img className="article-thumbnail" fluid={node.thumbnail[0]?.localFile.childImageSharp.fluid} />
+                  <div class="article-info-container">
+                    <h2><Link to={`${pageContext.parentPath}${pageContext.node.slug}/${node.slug}`}>{node.navigationTitle}</Link></h2>
+                  </div>
+                </div>
+              )
+            })}
+
             {pageContext.articles.map(function (node, key) {
               return (
                 <div class="article-container">
