07f56d6c5f0a17752dbf341f419646d5a416a390
Levi Sørum <levi.sorum@gmail.com>
2020-05-12 13:12:13 +0200

    Testing service worker in production.

--- src/sw.js
+++ src/sw.js
@@ -10,6 +10,14 @@ function createImageUrl(noApiUrl) {
   return result
 }
 
+function removeApiKey(apiUrl) {
+  var urlObject = new URL(apiUrl)
+  var searchParams = urlObject.searchParams
+  searchParams.delete('key')
+  urlObject.search = searchParams.toString()
+  return urlObject.toString()
+}
+
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(cacheNames).then(function (cache) {
@@ -72,9 +80,7 @@ self.addEventListener('install', function (event) {
 function redirect(request) {
   if (request.url.indexOf(`maps.googleapis.com/maps/api/staticmap`) > -1) {
     // Redirect to cached static map image
-    var urlObject = new URL(request.url)
-    urlObject.delete('key')
-    var path = createImageUrl(urlObject)
+    var path = createImageUrl(removeApiKey(request.url))
     console.log(`Redirecting from ${request.url} to ${path}`)
     return new Request(path, { mode: 'no-cors' })
   }
