2e61ef7085f2f15f159b07e2b6827f69143fe091
Levi Sørum <levi.sorum@gmail.com>
2020-06-22 11:59:54 +0200

    Styled logo

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -11,11 +11,17 @@ export default ({ layoutContext }) => {
     <BurgerMenu layoutContext={layoutContext}></BurgerMenu>
 
     <div className="logo-container">
-      <Link id="trondheimno-link" to={(layoutContext.locale === 'no') ? '/' : `/${layoutContext.locale.split('-')[0]}/`}>TRONDHEIM.NO</Link>
+      <Link id="trondheimno-link" to={(layoutContext.locale === 'no') ? '/' : `/${layoutContext.locale.split('-')[0]}/`}>
 
-      <div className="logo">
-        <Img fluid={layoutContext.logoImage.localFile.childImageSharp.fluid} alt="Trondheim logo" />
-      </div>
+        <div className="logo-text">TRONDHEIM.NO</div>
+
+        <div className="logo">
+          <Img fluid={layoutContext.logoImage.localFile.childImageSharp.fluid} alt="Trondheim logo" />
+        </div>
+
+      </Link>
+
+
     </div>
   </div>
 }
