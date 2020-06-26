455cb98d4208b1897abc83a651bb022598dffe76
Levi Sørum <levi.sorum@gmail.com>
2020-06-26 15:48:39 +0200

    Made menu responsive.

--- src/components/menu.js
+++ src/components/menu.js
@@ -44,6 +44,57 @@ export class BurgerMenu extends React.Component {
 
   render() {
 
+    const Overlay = () => {
+      return this.state.showMenu ? (<div id="menu-background-overlay" />) : null
+    }
+
+    const Menu = () => {
+      return this.state.showMenu
+        ? (
+          <div className="drop-menu-container">
+            <Link
+              key={0}
+              className="drop-menu-item-container"
+              to={this.props.layoutContext.homePath}>
+              {LocalizationHelper.getLocalWord(this.props.layoutContext.localization, 'home', this.props.layoutContext.locale)}
+            </Link>
+            {this.props.layoutContext.menuData.map(function (node, key) {
+              return (
+                <Link
+                  key={key}
+                  className="drop-menu-item-container"
+                  to={node.path}>
+                  {node.title}
+                </Link>
+              )
+            })}
+
+            <ExtraMenuOptions />
+
+            <Link
+              className="drop-menu-item-container"
+              to={(this.props.layoutContext.locale === 'no') ? this.props.layoutContext.localizedPaths.en : this.props.layoutContext.localizedPaths.no}>
+
+              <ReactCountryFlag
+                className="drop-menu-item-flag"
+                countryCode={(this.props.layoutContext.locale !== 'no') ? 'NO' : 'GB'}
+                svg
+                style={{
+                  width: '1em',
+                  height: '1em',
+                }}
+                title="flag" />
+
+              {(this.props.layoutContext.locale === 'no') ? `English` : `Norsk`}
+
+            </Link>
+          </div>
+        )
+        : (
+          null
+        )
+    }
+
     const ExtraMenuOptions = () => {
       if (this.props.layoutContext.navbar.childrenFlamelinkNavbarContentFieldExtraMenuOptionsItem) {
         return (<span>
@@ -72,54 +123,8 @@ export class BurgerMenu extends React.Component {
         <div className="menu-text-container">
           {LocalizationHelper.getLocalWord(this.props.layoutContext.localization, 'menu', this.props.layoutContext.locale)}
         </div>
-
-
-        {
-          this.state.showMenu
-            ? (
-              <div className="drop-menu-container">
-                <Link
-                  key={0}
-                  className="drop-menu-item-container"
-                  to={this.props.layoutContext.homePath}>
-                  {LocalizationHelper.getLocalWord(this.props.layoutContext.localization, 'home', this.props.layoutContext.locale)}
-                </Link>
-                {this.props.layoutContext.menuData.map(function (node, key) {
-                  return (
-                    <Link
-                      key={key}
-                      className="drop-menu-item-container"
-                      to={node.path}>
-                      {node.title}
-                    </Link>
-                  )
-                })}
-
-                <ExtraMenuOptions />
-
-                <Link
-                  className="drop-menu-item-container"
-                  to={(this.props.layoutContext.locale === 'no') ? this.props.layoutContext.localizedPaths.en : this.props.layoutContext.localizedPaths.no}>
-
-                  <ReactCountryFlag
-                    className="drop-menu-item-flag"
-                    countryCode={(this.props.layoutContext.locale !== 'no') ? 'NO' : 'GB'}
-                    svg
-                    style={{
-                      width: '1em',
-                      height: '1em',
-                    }}
-                    title="flag" />
-
-                  {(this.props.layoutContext.locale === 'no') ? `English` : `Norsk`}
-
-                </Link>
-              </div>
-            )
-            : (
-              null
-            )
-        }
+        <Menu />
+        <Overlay />
       </div>
     );
   }
