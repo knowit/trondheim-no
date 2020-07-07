fc9b4ad038f30a259bb98ae6c7c0ae0c6d8c24ba
Levi Sørum <levi.sorum@gmail.com>
2020-07-07 11:51:13 +0200

    Added timeout and image cache to trdEvents

--- src/sw.js
+++ src/sw.js
@@ -1,71 +1,52 @@
 // --------------------------------------------------------------------------
 
+
 var cacheNames = ['trd-events'];
 const trdEventsUrl = `https://us-central1-trdevents-224613.cloudfunctions.net/getNextEvents`
+const trdEventsUrlParameters = `?numEvents=20`
+const timeIntervalInMinutes = 60
+const timeoutInMillis = 1000 * 60 * timeIntervalInMinutes
 
-async function fetchTrdEventsJson(forceUpdate = false) {
 
-  console.log("Fetching trd-events...")
-  const cache = await caches.open(cacheNames)
-  const request = new Request(trdEventsUrl)
-  const response = await cache.match(request)
 
-  if (response && !forceUpdate) {
-    console.log("Retrieved cached trd-events.")
-    return await response.json()
-  }
+async function checkTrdEventsTimestamp() {
 
-  else {
-
-    const res = await fetch(request)
-    console.log("Fetch trd-events complete.")
-    cache.put(request, res)
-    return await res.json()
-  }
+  const timestamp = await idbKeyval.get('trd-events-timestamp')
+  return timestamp ? (new Date(Date.now())).getTime() - (new Date(timestamp)).getTime() < timeoutInMillis : false
 }
 
+async function setTrdEventsTimestamp() {
+  await idbKeyval.set('trd-events-timestamp', Date.now())
+}
 
-function initDatabase(data) {
+async function initDatabase(data) {
   console.log("Initializing IndexedDB...")
 
-  var request = self.indexedDB.open('TRD_EVENTS', 1)
+  var request = await self.indexedDB.open('TRD_EVENTS', 1)
 
   request.onsuccess = function (event) {
     console.log('[onsuccess]', request.result);
 
     var db = event.target.result; // === request.result
     var transaction = db.transaction('event', 'readwrite');
-    var varsTransaction = db.transaction('vars', 'readwrite');
 
     transaction.onsuccess = function (event) {
       console.log('[Transaction] Transaction complete');
     };
-    varsTransaction.onsuccess = function (event) {
-      console.log('[VarsTransaction] Transaction complete');
-    };
 
     var objectStore = transaction.objectStore('event');
 
-    var varsStore = varsTransaction.objectStore('vars');
-
-    data.filter(item => item.eventCancelled === false)
-      .slice(0, 10)
+    data
+      .map(item => {
+        delete item['repetitions']
+        return item
+      })
       .map(item => {
         var db_op_req = objectStore.add(item); // IDBRequest
         db_op_req.onsuccess = function (event) {
           console.log(event.target.result == item.event_slug); // true
         };
       })
-
-    var date_remove_req = varsStore.delete('date')
-    date_remove_req.onsuccess = function (event) {
-      console.log('Removed timestamp');
-    };
-
-    var date_add_req = varsStore.add({ name: 'date', value: Date.now() })
-    date_add_req.onsuccess = function (event) {
-      console.log('Added timestamp');
-    };
   };
 
   request.onerror = function (event) {
@@ -77,19 +58,49 @@ function initDatabase(data) {
     var db = event.target.result;
     var store = db.createObjectStore('event', { keyPath: 'event_slug' });
     store.createIndex('event_slug_unqiue', 'event_slug', { unique: true })
-
-    var store = db.createObjectStore('vars', { keyPath: 'name' });
-
   };
+}
 
+async function cacheEventImages(data, cache) {
+  data.map(async function (item) {
+    const request = new Request(item.imageURL)
+    const response = await fetch(item.imageURL, { mode: 'no-cors' })
+    cache.put(request, response)
+  })
+}
+
+async function fetchTrdEventsJson(cache, forceUpdate = false) {
+
+  console.log("Fetching trd-events...")
+  const request = new Request(trdEventsUrl)
+  const response = await cache.match(request, { ignoreSearch: true })
+  const timestamp = await checkTrdEventsTimestamp()
+
+  if (response && timestamp && !forceUpdate) {
+    console.log("Retrieved cached trd-events.")
+    return await response.json()
+  }
+
+  else {
+    const res = await fetch(`${trdEventsUrl}${trdEventsUrlParameters}`)
+    console.log("Fetch trd-events complete.")
+    cache.put(request, res.clone())
+
+    const result = await res.clone().json()
+    setTrdEventsTimestamp()
+    cacheEventImages(result, cache)
+    return result
+  }
 }
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(cacheNames).then(function (cache) {
-
       console.log("Service Worker: Installing")
-      fetchTrdEventsJson().then(data => initDatabase(data))
+      fetchTrdEventsJson(cache)
+        .then(data =>
+          initDatabase(data)
+        )
     })
   );
 });
