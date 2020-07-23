bb29bb88fd56f20df3ec65a01b860c07c87a25b0
Levi Sørum <levi.sorum@gmail.com>
2020-07-23 11:25:58 +0200

    Fixed search page.

--- src/templates/search-template.js
+++ src/templates/search-template.js
@@ -1,97 +1,108 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'gatsby'
 import Layout from "../layouts/layout"
 import "../style/search.css"
 import EllipsisText from "react-ellipsis-text"
 import { Router } from "@reach/router"
 
-class SearchComponent extends React.Component {
+const Search = ({ pageContext, location }) => {
+  const [query, setQuery] = useState('')
+  const [results, setResults] = useState([])
+  const [quantity, setQuantity] = useState(10)
+  const [pageNumber, setPageNumber] = useState(0)
+  const [locationHref, setLocationHref] = useState(location.href)
+  const antall = [5, 10, 20, 50, 100]
+  const locale = pageContext.locale.split('-')[0]
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      query: '',
-      results: [],
-      quantity: '10',
-      pageNumber: 0,
-      antall: [5, 10, 20, 50, 100]
-    }
 
-    this.setQuery = this.setQuery.bind(this)
-    this.setResults = this.setResults.bind(this)
-    this.setQuantity = this.setQuantity.bind(this)
-    this.setPageNumber = this.setPageNumber.bind(this)
-  }
-
-  setQuery(query) {
+  // Only re-run if query changes
+  useEffect(() => {
     if (!query || !window.__LUNR__) {
-      this.setResults([])
-      this.setState({ query: '' })
+      setResults([])
+      setQuery('')
       return
     }
-    this.setState({ query: query })
-    const lunrIndex = window.__LUNR__[this.props.pageContext.locale.split('-')[0]]
+
+    const lunrIndex = window.__LUNR__[locale]
     const searchResults = lunrIndex.index.search(query)
-    this.setResults(
+    setResults(
       searchResults.map(({ ref }) => {
         return lunrIndex.store[ref]
       })
     )
-  }
 
-  setResults(results) {
-    this.setState({ results: results })
-  }
-
-  setQuantity(quantity) {
-    this.setState({ quantity: quantity })
-    const maxPage = Math.ceil(this.state.results.length / quantity)
-    if (this.state.pageNumber >= maxPage) {
-      this.setState({ pageNumber: maxPage - 1 })
+    if (quantity === 'all' || quantity === 'alle' || pageNumber < 0) {
+      setPageNumber(0)
     }
+    else if (pageNumber !== 0) {
+      const maxPage = Math.ceil(results.length / quantity)
+      if (pageNumber >= maxPage) {
+        setPageNumber(maxPage - 1)
+      }
+    }
+  }, [query])
+
+  // Make sure page number is valid upon change
+  useEffect(() => {
+    if (quantity === 'all' || quantity === 'alle' || pageNumber < 0) {
+      setPageNumber(0)
+    }
+    else if (pageNumber != 0) {
+      const maxPage = Math.ceil(results.length / quantity)
+      if (pageNumber >= maxPage) {
+        setPageNumber(maxPage - 1)
+      }
+    }
+  }, [quantity, pageNumber])
+
+  const maxPageNumber = () => {
+    return Math.ceil(results.length / quantity)
   }
 
-  setPageNumber(number) {
-    this.setState({ pageNumber: number })
-  }
+  // Only re-run if locationHref changes
+  useEffect(() => {
+    const url = new URL(locationHref)
 
-  componentDidMount() {
-    const url = new URL(this.props.location.href)
-    console.log(url)
-    url.searchParams.forEach((value, key, parent) => {
-      console.log(`key: ${key}, value: ${value}`)
-    })
+    const urlQuantity = url.searchParams.get('quantity')
+    const urlQuery = url.searchParams.get('query')
+    const urlPage = url.searchParams.get('page')
 
-    const quantity = url.searchParams.get('quantity')
-    const query = url.searchParams.get('query')
-    const page = url.searchParams.get('page')
-
-    if (query) {
-      this.setQuery(query)
+    if (urlQuery != null) {
+      setQuery(urlQuery)
+      const lunrIndex = window.__LUNR__[locale]
+      const searchResults = lunrIndex.index.search(urlQuery)
+      setResults(
+        searchResults.map(({ ref }) => {
+          return lunrIndex.store[ref]
+        })
+      )
     }
-    if (quantity) {
-      this.setQuantity(quantity)
+    if (urlQuantity != null) {
+      setQuantity(urlQuantity)
     }
-    if (page && page > 0) {
-      this.setPageNumber(page - 1)
+    if (urlPage != null) {
+      setPageNumber(urlPage - 1)
     }
+  },
+    [locationHref])
 
-  }
 
-  splitSearchResults() {
-    var pageLength = this.state.quantity
+
+
+  const splitSearchResults = () => {
+    var pageLength = quantity
     var pages = []
     var currentPage = []
     var index = 1
 
-    if (this.state.quantity === 'all') {
-      pages.push(this.state.results.map(r => {
+    if (quantity === 'all') {
+      pages.push(results.map(r => {
         return { index: index++, value: r }
       }))
     }
 
     else {
-      this.state.results.map(r => {
+      results.map(r => {
         currentPage.push({ index: index++, value: r })
         if (currentPage.length >= pageLength) {
           pages.push(currentPage)
@@ -106,177 +117,172 @@ class SearchComponent extends React.Component {
     return pages
   }
 
-  maxPageNumber() {
-    return Math.ceil(this.state.results.length / this.state.quantity)
-  }
+  const SearchResultQuantityText = ({ children }) => (
+    <div id="search-result-quantity-container">
+      <span>{`${pageContext.locale === 'no' ? 'Antall' : 'Total'}: `}</span>
+      {children}
+      <span>{pageContext.locale === 'no' ? ' resultater funnet.' : ' results found.'}</span>
+    </div>
+  )
 
-
-  render() {
-
-    const SearchResultQuantityText = ({ children }) => (
-      <div id="search-result-quantity-container">
-        <span>{`${this.props.pageContext.locale === 'no' ? 'Antall' : 'Total'}: `}</span>
-        {children}
-        <span>{this.props.pageContext.locale === 'no' ? ' resultater funnet.' : ' results found.'}</span>
+  const SearchResultQuantity = () => (
+    <div id="search-result-quantity-number-container" >
+      <div id="search-result-quantity-number">
+        {results.length}
       </div>
-    )
+    </div>
+  )
 
-    const SearchResultQuantity = () => (
-      <div id="search-result-quantity-number-container" >
-        <div id="search-result-quantity-number">
-          {this.state.results.length}
-        </div>
-      </div>
-    )
+  const SearchResultItem = ({ url, title, content, index }) => (
+    <div className="search-result-item">
+      <b>{index}.
+      <Link to={url}>
+          <EllipsisText className="search-result-content-text" text={title} length={100} />
+        </Link>
+      </b>
+      <p>
+        <EllipsisText className="search-result-content-text" text={content} length={250} />
+      </p>
+    </div>
+  )
 
-    const SearchResultItem = ({ url, title, content, index }) => (
-      <div className="search-result-item">
-        <b>{index}.
-        <Link to={url}>
-            <EllipsisText className="search-result-content-text" text={title} length={100} />
-          </Link>
-        </b>
-        <p>
-          <EllipsisText className="search-result-content-text" text={content} length={250} />
-        </p>
-      </div>
-    )
-
-    const SearchResultsPage = ({ page }) => {
-      return (
-        <div>
-          {
-            (this.state.quantity !== 'all')
-              ? (<div id="page-number-text">
-                {this.props.pageContext.locale === 'no' ? 'Side ' : 'Page '}
-                {this.state.pageNumber + 1}
-                {this.props.pageContext.locale === 'no' ? ' av ' : ' of '}
-                {this.maxPageNumber()}
-              </div>)
-              : (<br />)
-          }
-          {page.map(r => (
-            <SearchResultItem
-              key={r.index}
-              url={r.value.url}
-              title={r.value.title}
-              content={r.value.content}
-              index={r.index} />
-          ))}
-        </div>
-      )
-    }
-
-    const SearchResults = () => {
-      const pages = this.splitSearchResults()
-      const page = pages[this.state.pageNumber] ? pages[this.state.pageNumber] : []
-      var index = 0
-      return (
-        <div>
-          <SearchResultsPage page={page} />
-          {this.state.quantity === 'all' || pages.length <= 1 ? null : (
-            <div id="search-page-buttons-container">
-              {
-                this.state.pageNumber > 0
-                  ? (
-                    <button key={'first'} className="other-page-button"
-                      onClick={e => this.setPageNumber(0)}>
-                      {this.props.pageContext.locale === 'no' ? 'Første' : 'First'}
-                    </button>
-                  ) : null
-              }
-              {
-                this.state.pageNumber > 0
-                  ? (
-                    <button key={'previous'} className="other-page-button"
-                      onClick={e => this.setPageNumber(this.state.pageNumber - 1)}>
-                      {this.props.pageContext.locale === 'no' ? 'Forrige' : 'Previous'}
-                    </button>
-                  ) : null
-              }
-              {pages.map(p => {
-                return {
-                  index: index++,
-                  value: p
-                }
-              }).map(p => {
-                return (
-                  <button key={p.index}
-                    id={this.state.pageNumber === p.index ? 'selected-page-button' : null}
-                    className="page-num-button"
-                    onClick={e => this.setPageNumber(p.index)}>
-                    {p.index + 1}
-                  </button>
-                )
-              })}
-              {
-                this.state.pageNumber < this.maxPageNumber() - 1
-                  ? (
-                    <button key={'next'} className="other-page-button"
-                      onClick={e => this.setPageNumber(this.state.pageNumber + 1)}>
-                      {this.props.pageContext.locale === 'no' ? 'Neste' : 'Next'}
-                    </button>
-                  ) : null
-              }
-              {
-                this.state.pageNumber < this.maxPageNumber() - 1
-                  ? (
-                    <button key={'last'} className="other-page-button"
-                      onClick={e => this.setPageNumber(this.maxPageNumber() - 1)}>
-                      {this.props.pageContext.locale === 'no' ? 'Siste' : 'Last'}
-                    </button>
-                  ) : null
-              }
-            </div>
-          )}
-        </div>)
-    }
-
-
+  const SearchResultsPage = ({ page }) => {
     return (
-      <div id="search-component-container" >
-
-        <div id="search-input-container">
-          <input
-            type='text'
-            defaultValue={this.state.query}
-            onChange={event =>
-              this.setQuery(event.target.value)
-            }
-          />
-        </div>
-
-        <SearchResultQuantityText>
-          <SearchResultQuantity />
-        </SearchResultQuantityText>
-
-        <div id="search-quantity-select">
-          <span>
-            {this.props.pageContext.locale === 'no' ? 'Vis antall:' : 'Display quantity:'}
-          </span>
-          <select
-            value={this.state.quantity}
-            onChange={e => this.setQuantity(e.target.value)}>
-            {this.state.antall.map(n => (
-              <option key={n} value={n}>{n}</option>
-            ))}
-            <option key={'all'} value={'all'}>
-              {this.props.pageContext.locale === 'no' ? 'Alle' : 'All'}
-            </option>
-          </select>
-        </div>
-
-        <SearchResults />
-
+      <div>
+        {
+          (quantity !== 'all')
+            ? (<div id="page-number-text">
+              {pageContext.locale === 'no' ? 'Side ' : 'Page '}
+              {pageNumber + 1}
+              {pageContext.locale === 'no' ? ' av ' : ' of '}
+              {maxPageNumber()}
+            </div>)
+            : (<br />)
+        }
+        {page.map(r => (
+          <SearchResultItem
+            key={r.index}
+            url={r.value.url}
+            title={r.value.title}
+            content={r.value.content}
+            index={r.index} />
+        ))}
       </div>
     )
   }
+
+  const SearchResults = () => {
+    const pages = splitSearchResults()
+    const page = pages[pageNumber] ? pages[pageNumber] : []
+    var index = 0
+    return (
+      <div>
+        <SearchResultsPage page={page} />
+        {quantity === 'all' || pages.length <= 1 ? null : (
+          <div id="search-page-buttons-container">
+            {
+              pageNumber > 0
+                ? (
+                  <button key={'first'} className="other-page-button"
+                    onClick={e => setPageNumber(0)}>
+                    {pageContext.locale === 'no' ? 'Første' : 'First'}
+                  </button>
+                ) : null
+            }
+            {
+              pageNumber > 0
+                ? (
+                  <button key={'previous'} className="other-page-button"
+                    onClick={e => setPageNumber(pageNumber - 1)}>
+                    {pageContext.locale === 'no' ? 'Forrige' : 'Previous'}
+                  </button>
+                ) : null
+            }
+            {pages.map(p => {
+              return {
+                index: index++,
+                value: p
+              }
+            }).map(p => {
+              return (
+                <button key={p.index}
+                  id={pageNumber === p.index ? 'selected-page-button' : null}
+                  className="page-num-button"
+                  onClick={e => setPageNumber(p.index)}>
+                  {p.index + 1}
+                </button>
+              )
+            })}
+            {
+              pageNumber < maxPageNumber() - 1
+                ? (
+                  <button key={'next'} className="other-page-button"
+                    onClick={e => setPageNumber(pageNumber + 1)}>
+                    {pageContext.locale === 'no' ? 'Neste' : 'Next'}
+                  </button>
+                ) : null
+            }
+            {
+              pageNumber < maxPageNumber() - 1
+                ? (
+                  <button key={'last'} className="other-page-button"
+                    onClick={e => setPageNumber(maxPageNumber() - 1)}>
+                    {pageContext.locale === 'no' ? 'Siste' : 'Last'}
+                  </button>
+                ) : null
+            }
+          </div>
+        )}
+      </div>)
+  }
+
+
+
+  return (
+    <div id="search-component-container" >
+
+      <div id="search-input-container">
+        <input
+          type='text'
+          defaultValue={query}
+          onChange={event =>
+            setQuery(event.target.value)
+          }
+        />
+      </div>
+
+      <SearchResultQuantityText>
+        <SearchResultQuantity />
+      </SearchResultQuantityText>
+
+      <div id="search-quantity-select">
+        <span>
+          {pageContext.locale === 'no' ? 'Vis antall:' : 'Display quantity:'}
+        </span>
+        <select
+          value={quantity}
+          onChange={e => setQuantity(e.target.value)}>
+          {antall.map(n => (
+            <option key={n} value={n}>{n}</option>
+          ))}
+          <option key={'all'} value={'all'}>
+            {pageContext.locale === 'no' ? 'Alle' : 'All'}
+          </option>
+        </select>
+      </div>
+
+      <SearchResults />
+
+    </div>
+  )
+
+
 }
+
 
 const path = (locale) => (
   locale === 'no' ? '/search' : '/en/search'
 )
-
 
 export default ({ pageContext, location }) => {
   return (
@@ -285,7 +291,7 @@ export default ({ pageContext, location }) => {
         <div id="inner-container">
 
           <Router basepath={path(pageContext.locale)}>
-            <SearchComponent path={'/'} pageContext={pageContext} location={location} />
+            <Search path={'/'} pageContext={pageContext} location={location} />
           </Router>
 
         </div>
