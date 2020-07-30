f9881991f5fd117bf8abb884da1aeca06b057f54
Levi Sørum <levi.sorum@gmail.com>
2020-07-30 10:18:55 +0200

    Fixed missing menu options

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,157 +4,27 @@ import { Link, graphql, StaticQuery } from 'gatsby';
 import ReactCountryFlag from "react-country-flag"
 import LocalizationHelper from "../helpers/helpers"
 
-/*
-class BurgerMenu extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showMenu: false,
-    };
-
-    this.showMenu = this.showMenu.bind(this);
-    this.closeMenu = this.closeMenu.bind(this);
-  }
-
-  showMenu(event) {
-    //event.preventDefault();
-
-    this.setState({ showMenu: true }, () => {
-      document.addEventListener('click', this.closeMenu);
-    });
-  }
-
-  closeMenu() {
-    this.setState({ showMenu: false }, () => {
-      document.removeEventListener('click', this.closeMenu);
-    });
-  }
-
-  toggleMenu(e) {
-    if (this.state.showMenu) {
-      this.closeMenu()
-    }
-    else {
-      this.showMenu(e)
-    }
-  }
-
-
-  render() {
-
-    const Overlay = () => {
-      return this.state.showMenu ? (<div id="menu-background-overlay" />) : null
-    }
-
-    const Menu = () => {
-      return this.state.showMenu
-        ? (
-          <div className="drop-menu-container">
-            <Link
-              key={0}
-              role="menuitem"
-              className="drop-menu-item-container"
-              to={this.props.layoutContext.homePath}>
-              {LocalizationHelper.getLocalWord(this.props.layoutContext.localization, 'home', this.props.layoutContext.locale)}
-            </Link>
-            {this.props.layoutContext.menuData.map(function (node, key) {
-              return (
-                <Link
-                  key={key}
-                  role="menuitem"
-                  className="drop-menu-item-container"
-                  to={node.path}>
-                  {node.title}
-                </Link>
-              )
-            })}
-
-            <ExtraMenuOptions />
-
-            <Link
-              className="drop-menu-item-container"
-              role="menuitem"
-              to={this.props.layoutContext.localizedPaths.find(item => item.locale !== this.props.layoutContext.locale).path}>
-
-              <ReactCountryFlag
-                className="drop-menu-item-flag"
-                countryCode={(this.props.layoutContext.locale !== 'no') ? 'NO' : 'GB'}
-                svg
-                style={{
-                  width: '1em',
-                  height: '1em',
-                }}
-              />
-
-              {(this.props.layoutContext.locale === 'no') ? `English` : `Norsk`}
-
-            </Link>
-          </div>
-        )
-        : (
-          null
-        )
-    }
-
-    const ExtraMenuOptions = () => {
-      if (this.props.layoutContext.navbar.childrenFlamelinkNavbarContentFieldExtraMenuOptionsItem) {
-        return (<span>
-          {this.props.layoutContext.navbar.childrenFlamelinkNavbarContentFieldExtraMenuOptionsItem.map(function (node, key) {
-            return (
-              <a
-                key={key}
-                role="menuitem"
-                className="drop-menu-item-container"
-                href={node.redirectUrl} target="_blank" rel="noreferrer">
-                {node.title}
-              </a>
-            )
-          })}
-        </span>)
-      }
-      else return null
-    }
-
-    return (
-      <div role="menu" tabIndex={0} onKeyPress={this.toggleMenu.bind(this)} className="menu-container" onClick={this.showMenu}>
-        <div className="burger-container">
-          <div className="burger-bar"></div>
-          <div className="burger-bar"></div>
-          <div className="burger-bar"></div>
-        </div>
-        <div className="menu-text-container">
-          {LocalizationHelper.getLocalWord(this.props.layoutContext.localization, 'menu', this.props.layoutContext.locale)}
-        </div>
-        <Menu />
-        <Overlay />
-      </div>
-    );
-  }
-}
-*/
-
-const Menu = () => {
+const Menu = ({ layoutContext }) => {
 
   const query = graphql`
-query MenuQuery {
-  allMenuDataContent {
-    edges {
-      node {
-        id
-        locale
-        menuItems {
-          locale
-          title
-          slug
-          path
+    query MenuQuery {
+      allMenuDataContent {
+        edges {
+          node {
+            id
+            locale
+            menuItems {
+              locale
+              title
+              slug
+              path
+            }
+          }
         }
       }
     }
-  }
-}
-`
+    `
   const [showMenu, setShowMenu] = useState(false)
 
   const toggleMenu = () => {
@@ -166,7 +36,6 @@ query MenuQuery {
   }
 
   const MenuItems = ({ items }) => {
-    console.log(items)
     return showMenu
       ? (
         <div className="drop-menu-container">
@@ -181,6 +50,24 @@ query MenuQuery {
               </Link>
             )
           })}
+          <Link
+            className="drop-menu-item-container"
+            role="menuitem"
+            to={layoutContext.localizedPaths.find(item => item.locale !== layoutContext.locale).path}>
+
+            <ReactCountryFlag
+              className="drop-menu-item-flag"
+              countryCode={(layoutContext.locale !== 'no') ? 'NO' : 'GB'}
+              svg
+              style={{
+                width: '1em',
+                height: '1em',
+              }}
+            />
+
+            {(layoutContext.locale === 'no') ? `English` : `Norsk`}
+
+          </Link>
         </div>
       )
       : (
@@ -191,6 +78,7 @@ query MenuQuery {
   return (
     <StaticQuery query={query}
       render={(data) => {
+        const menuData = data.allMenuDataContent.edges.find(node => node.node.locale === layoutContext.locale).node
         return (
           <div role="menu" tabIndex={0} onKeyPress={toggleMenu} className="menu-container" onClick={toggleMenu}>
             <div className="burger-container">
@@ -199,9 +87,9 @@ query MenuQuery {
               <div className="burger-bar"></div>
             </div>
             <div className="menu-text-container">
-              MENY
+              {LocalizationHelper.getLocalWord(layoutContext.localization, 'menu', layoutContext.locale)}
             </div>
-            <MenuItems items={data.allMenuDataContent.edges[0].node.menuItems} />
+            <MenuItems items={menuData ? menuData.menuItems : []} />
             <Overlay />
           </div>
         )
@@ -209,7 +97,7 @@ query MenuQuery {
       }
     />
   );
-
 }
+
 
 export default Menu
