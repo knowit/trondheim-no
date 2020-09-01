5f6e64be1924f57246c2e211da5ab3a4fd46eb7c
Christopher Gjøvåg <christopher.gjovag@knowit.no>
2020-09-01 16:02:43 +0200

    Added basics of new main menu

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import "../style/layout.css"
 import { Helmet } from "react-helmet"
 import { Link, graphql, useStaticQuery } from "gatsby"
+import className from "classnames";
 
 export default ({ children, locale, localizedPaths }) => {
   const data = useStaticQuery(graphql`
@@ -64,11 +65,37 @@ export default ({ children, locale, localizedPaths }) => {
           }
         }
       }
+
+      allFlamelinkSidebarMainMenuNavigation {
+        nodes {
+          id
+          flamelink_locale
+          items {
+            uuid
+            title
+            url
+            root
+            flamelink_children {
+              uuid
+              title
+              url
+              root
+            }
+          }
+        }
+      }
     }
   `)
 
   const menuItems = data.menu.edges.find((it) => it.node.locale === locale).node
     .menuItems
+
+  const sidebarMainMenu = data.allFlamelinkSidebarMainMenuNavigation.nodes.find(
+    (it) => it.flamelink_locale === locale
+  ).items
+
+  const re = /^(?:\/en)?\/([\w\-\_]+)(?:\/\S+|$)/
+  const root = re.exec(window.location.pathname)[1]
 
   const Navigation = ({ items }) => {
     return (
@@ -89,6 +116,49 @@ export default ({ children, locale, localizedPaths }) => {
     )
   }
 
+  const NavItem = ({ item }) => {
+    const SubItems = ({ show }) => {
+      if (item.flamelink_children && item.flamelink_children.length && !!show) {
+        return (
+          <div className="child-item-container">
+            {item.flamelink_children.map((it) => {
+              return (
+                <Link to={it.url} className="child-nav">
+                  {it.title}
+                </Link>
+              )
+            })}
+          </div>
+        )
+      }
+      return null
+    }
+
+    const cn = className({
+      active: item.root === root,
+      'nav-item-container': true,
+    })
+
+    return (
+      <div className={cn}>
+        <Link to={item.url}>
+          {item.title}
+        </Link>
+        <SubItems show={item.root === root} />
+      </div>
+    )
+  }
+
+  const Menu = ({ id, items }) => {
+    return (
+      <div id={id} className="navigation-container">
+        {items.map((item) => {
+          return <NavItem item={item} />
+        })}
+      </div>
+    )
+  }
+
   return (
     <div id="layout-container">
       <Helmet title="Trondheim.no">
@@ -98,8 +168,12 @@ export default ({ children, locale, localizedPaths }) => {
           content="The official website for Trondheim."
         />
       </Helmet>
-      <Navigation id="navbar" items={menuItems} />
-      <div id="children-container">{children}</div>
+      <header id="header">
+        <button>menu</button>
+        <p>TRONDHEIM</p>
+      </header>
+      <Menu id="menu" items={sidebarMainMenu} />
+      <div id="layout-content-container">{children}</div>
     </div>
   )
 }
